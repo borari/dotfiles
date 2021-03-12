47414b3f9e2af2ca351501228488b1ed051938a6
borari <borari@fubared.net>
2021-03-12 18:43:02 -0500

    fixed wrong source

--- autorecon/install.sh
+++ autorecon/install.sh
@@ -6,9 +6,10 @@ if test "$(uname -s)" = "Linux"
 then
   if test ! $(which autorecon)
   then
+    echo " Installing AutoRecon dependencies first!"
+    apt install seclists curl enum4linux gobuster nbtscan nikto nmap onesixtyone oscanner smbclient smbmap smtp-user-enum snmp sslscan sipvicious tnscmd10g whatweb wkhtmltopdf -y
     echo "  Installing AutoRecon for you."
-    python3 -m pipx install autorecon
-    echo "  Installing tree for you."
+    python3 -m pipx install git+https://github.com/Tib3rius/AutoRecon.git
   fi
 fi
 
