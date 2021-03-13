edef87067b13fb4eca84403832ab137f6aec1072
borari <borari@fubared.net>
2021-03-12 20:53:38 -0500

    fixed updatedb alias on linux

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -2,4 +2,8 @@ alias sudo='sudo env \"PATH=$PATH\"'
 alias la-'ls -lAh --color'
 alias ls1='ls -1 --color'
 alias ll='ls -lh --color'
-alias updatedb='sudo /usr/libexec/locate.updatedb'
+
+if test "$(uname)" = "Darwin"
+then
+  alias updatedb='sudo /usr/libexec/locate.updatedb'
+fi
