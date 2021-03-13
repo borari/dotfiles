101a46ff00a14cfb25dba3ed8eb9544c757075be
borari <borari@fubared.net>
2021-03-12 20:18:22 -0500

    fixed typo

--- ssh/keys.zsh
+++ ssh/keys.zsh
@@ -1,10 +1,10 @@
 # Pipe public key to clipboard, sourcing from /root/.ssh as needed for me on Kali.
-if [ "$(uname -s)" == "Darwin" ]
+if [ "$(uname -s)" = "Darwin" ]
 then
   alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
-elif [ "$(uname -s)" == "Linux" ]
+elif [ "$(uname -s)" = "Linux" ]
 then
-  if [ "$(lsb_release -c)" == "Codename:	kali-rolling"
+  if [ "$(lsb_release -c)" = "Codename:	kali-rolling"
   then
     alias pubkey="more /root/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
   else
