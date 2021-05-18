7b268f5014ae33cb5c45b5a6a1ac0fab94fd3b91
borari <borari@fubared.net>
2021-05-18 14:34:28 -0400

    fixed else syntax

--- ssh/keys.zsh
+++ ssh/keys.zsh
@@ -4,6 +4,7 @@
 if test "$(uname -s)" = "Darwin"
 then
   alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
-elif test "$(uname -s)" = "Linux"
+else test "$(uname -s)" = "Linux"
   alias pubkey="more ~/.ssh/id_rsa.pub"
 fi
+
