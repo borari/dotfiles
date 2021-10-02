0ae87f6a8211ee56bd01a74d59e886b80672d983
borari <borari@fubared.net>
2021-10-02 13:48:58 -0400

    ovpn alias uses pass.txt

--- openvpn/aliases.zsh
+++ openvpn/aliases.zsh
@@ -1,4 +1,4 @@
 alias disconnect-vpn="killall openvpn"
 alias htb-vpn='openvpn $CTF/htb/borari.ovpn &'
-alias offsec-vpn-oscp='openvpn $OFFSEC/oscp/lab-connection/offsec.ovpn &'
-alias offsec-vpn-pg='openvpn $OFFSEC/proving_grounds/vpn/vpn_config.ovpn &'
+alias offsec-vpn-oscp='openvpn --config $OFFSEC/oscp/lab-connection/offsec.ovpn --auth-user-pass $OFFSEC/oscp/lab-connection/pass.txt &'
+alias offsec-vpn-pg='openvpn --config $OFFSEC/proving_grounds/vpn/vpn_config.ovpn --auth-user-pass $OFFSEC/proving_grounds/vpn/pass.txt &'
